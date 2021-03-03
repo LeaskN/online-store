719d389204730e00f0ba23c9a68f88b40b66dc4b
LeaskN <leask.nicholas@gmail.com>
2021-03-03 12:31:38 -0500

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,60 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import products from '../../products.json';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Ali's Jewles</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://app.snipcart.com" />
+        <link rel="preconnect" href="https://cdn.snipcart.com" />
+        <link rel="stylesheet" href="https://cdn.snipcart.com/themes/v3.0.21/default/snipcart.css" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to Ali's Jewles
+        </h1>
+        <p>Here you can find anything from handmade earrings to plant cuttings!</p>
+
+        <p className={styles.description}>
+          <a className="snipcart-checkout snipcart-summary" href="#" style={{ textDecoration: "none" }}>
+            <strong>Cart:</strong> <span className="snipcart-total-price">$0.00</span>
+          </a>
+        </p>
+
+        <div className={styles.grid}>
+          {products.map(product => {
+            return (
+              <div key={product.id} className={styles.card}>
+                <img src={product.image} alt={`Preview of ${product.title}`} />
+                <h3>{product.title}</h3>
+                <p>{product.description}</p>
+                <p>${product.price}</p>
+                <p>
+                  <button className="snipcart-add-item"
+                    data-item-id={product.id}
+                    data-item-image={product.image}
+                    data-item-name={product.title}
+                    data-item-url="/"
+                    data-item-price={product.price}
+                  >
+                    Add to Cart
+                </button>
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      </main>
+
+      <footer className={styles.footer}></footer>
+
+      <script async src="https://cdn.snipcart.com/themes/v3.0.21/default/snipcart.js" />
+      <div hidden={true} id="snipcart" data-api-key="NDBmMTc3OWItYTE4Ni00YTdhLWI0MzAtMjI1ZThmNWZmN2M4NjM3NTAzODQ4NzM3NDA1MDc3" />
+
+    </div>
+  )
+}
